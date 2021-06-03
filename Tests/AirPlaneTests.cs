004432deb2d85f2822678211e22f78f36c9ab913
Foxed1337 <48722259+Foxed1337@users.noreply.github.com>
2021-06-03 21:45:12 +0500

    Add files via upload

---
+++ Tests/AirPlaneTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using SaveEarth.MainClasses;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AirPlaneTests
+    {
+        [Test]
+        public void Checking_The_Corectness_Of_The_Constructor()
+        {
+            new AirPlane(0, 10, 2).Should().NotBeNull();
+            new AirPlane(5, 12, 6).Should().NotBeNull();
+            new AirPlane(5, 22, 0).Should().NotBeNull();
+            new AirPlane(5, 0, 82).Should().NotBeNull();
+        }
+
+        [Test]
+        public void Must_Turn_Left_when_Turn_Is_Left()
+        {
+            double CheckRadius = 10;
+            double CheckDirection = 5;
+            double CheckDeltaTime = 0.5;
+            double AirTurnVelocity = 0.15;
+            var air = new AirPlane(CheckRadius, CheckDirection, 0);
+            air.Move(Turn.Left, CheckDeltaTime);
+
+
+            CheckDirection += -AirTurnVelocity * CheckDeltaTime;
+            var LocX = -CheckRadius * Math.Sin(CheckDirection);
+            var LocY = CheckRadius * Math.Cos(CheckDirection);
+
+            air.Direction.Should().Be(CheckDirection);
+            air.LocationX.Should().Be(LocX);
+            air.LocationY.Should().Be(LocY);
+        }
+
+        [Test]
+        public void Must_Turn_Right_when_Turn_Is_Right()
+        {
+            double CheckRadius = 20;
+            double CheckDirection = 15;
+            double CheckDeltaTime = 0.7;
+            double AirTurnVelocity = 0.15;
+            var air = new AirPlane(CheckRadius, CheckDirection, 0);
+            air.Move(Turn.Right, CheckDeltaTime);
+
+
+            CheckDirection += AirTurnVelocity * CheckDeltaTime;
+            var LocX = -CheckRadius * Math.Sin(CheckDirection);
+            var LocY = CheckRadius * Math.Cos(CheckDirection);
+
+            air.Direction.Should().Be(CheckDirection);
+            air.LocationX.Should().Be(LocX);
+            air.LocationY.Should().Be(LocY);
+        }
+
+    }
+}
---
+++ Tests/EarthTests.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using SaveEarth.MainClasses;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Tests
+{
+    [TestFixture]
+    public class EarthTests
+    {      
+        [Test]
+        public void Earth_Should_Died_When_Damage_Is_Equal_EarthHP()
+        {
+            new Earth(0, 0, 1);
+            var a1 = new Earth(0, 0, 1);
+            a1.TakeDamage(1);
+            a1.isDead.Should().BeTrue();
+
+            var a2 = new Earth(0, 0, 10);
+            a2.TakeDamage(10);
+            a2.isDead.Should().BeTrue();
+
+            var a3 = new Earth(0, 0, 1000);
+            a3.TakeDamage(1000);
+            a3.isDead.Should().BeTrue();
+
+            var a4 = new Earth(0, 0, 500);
+            a4.TakeDamage(500);
+            a4.isDead.Should().BeTrue();
+
+            var a5 = new Earth(0, 0, 1000000);
+            a5.TakeDamage(1000000);
+            a5.isDead.Should().BeTrue();
+        }
+
+
+        [Test]
+        public void Earth_Should_Died_When_Damage_Is_Bigger_Then_EarthHP()
+        {
+            var a1 = new Earth(0, 0, 1);
+            a1.TakeDamage(110000);
+            a1.isDead.Should().BeTrue();
+
+            var a2 = new Earth(0, 0, 10);
+            a2.TakeDamage(10000);
+            a2.isDead.Should().BeTrue();
+
+            var a3 = new Earth(0, 0, 1000);
+            a3.TakeDamage(10000);
+            a3.isDead.Should().BeTrue();
+
+            var a4 = new Earth(0, 0, 500);
+            a4.TakeDamage(1000);
+            a4.isDead.Should().BeTrue();
+
+            var a5 = new Earth(0, 0, 1000000);
+            a5.TakeDamage(11000000);
+            a5.isDead.Should().BeTrue();         
+        }
+
+        [Test]
+        public void Earth_Shouldnt_Died_When_Damage_Is_Less_Then_EarthHP()
+        {
+            var a1  = new Earth(0, 0, 2);
+            a1.TakeDamage(1);
+            a1.isDead.Should().BeFalse();
+
+            var a2 = new Earth(0, 0, 10);
+            a2.TakeDamage(7);
+            a2.isDead.Should().BeFalse();
+
+            var a3 = new Earth(0, 0, 1000);
+            a3.TakeDamage(100);
+            a3.isDead.Should().BeFalse();
+
+            var a4 = new Earth(0, 0, 500);
+            a4.TakeDamage(50);
+            a4.isDead.Should().BeFalse();
+
+            var a5 = new Earth(0, 0, 1000000);
+            a5.TakeDamage(1000);
+            a5.isDead.Should().BeFalse();
+        }
+    }
+}
